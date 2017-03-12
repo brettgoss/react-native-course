1ddb3ea2806762d6c2ef2d7d2e6ed8271445e3a4
Brett Goss <brettgoss@live.com>
2017-03-12 14:56:51 -0700

    Create header component

---
+++ albums/src/components/header.js
@@ -0,0 +1,13 @@
+// Header Component
+
+// Import Libs
+import React from 'react';
+import { Text } from 'react-native';
+
+// Make Component
+const Header = () => {
+  return <Text>Albums!</Text>;
+};
+
+// Make Component available to rest of App
+export default Header;
